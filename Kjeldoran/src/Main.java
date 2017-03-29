743d293ec61768fb9d72b279bef131e2a8651d79
Phoenix <Phoenix@Lamborghini>
2017-03-29 17:31:25 -0400

    test1

--- Kjeldoran/src/Main.java
+++ Kjeldoran/src/Main.java
@@ -1,5 +1,6 @@
 import javax.swing.JOptionPane;
 
+import Utilities.Info;
 import entities.BasicEntity;
 import entities.Goblin;
 import entities.Player;
@@ -8,11 +9,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Info information = new Info();
+		
 		BasicEntity playah = new Player();
 		BasicEntity gob = new Goblin();
 	
-//String poop = JOptionPane.showInputDialog("Hello!");
-JOptionPane.showMessageDialog(null, "Kjeldoran ver. a.0.1 \nCopyright: Phoenix Feather Studios, 2017");
+//String test = JOptionPane.showInputDialog("Hello!");
+JOptionPane.showMessageDialog(null, "Kjeldoran ver. " + information.VERSION +"\nCopyright: Phoenix Feather Studios, 2017", "Kjeldoran, " + information.VERSION , JOptionPane.INFORMATION_MESSAGE);
 
 	}
 
